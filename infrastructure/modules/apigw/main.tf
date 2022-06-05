73511a42474f1d3c4ab3bf94880a9168efb9da12
kenko <universeengineer@gmail.com>
2022-06-05 22:23:24 +0900

    :ambulance: add triggers

--- infrastructure/modules/apigw/main.tf
+++ infrastructure/modules/apigw/main.tf
@@ -33,10 +33,14 @@ resource "aws_lambda_permission" "apigw" {
 }
 
 resource "aws_api_gateway_deployment" "_" {
+  rest_api_id       = aws_api_gateway_rest_api._.id
+  stage_name        = var.stage_name
+  
+  triggers = {
+    "hash" = filemd5("${path.module}/main.tf")
+    "lambda" = var.lambda.last_modified
+  }
   depends_on = [
     aws_api_gateway_integration._,
   ]
-  rest_api_id       = aws_api_gateway_rest_api._.id
-  stage_name        = var.stage_name
-  stage_description = "HASH=${filesha256("${path.module}/main.tf")}"
 }
