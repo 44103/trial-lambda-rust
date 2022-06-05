1f992a8756868d0661e0c9b0b8c89167186980f6
kenko <universeengineer@gmail.com>
2022-06-05 22:21:32 +0900

    :recycle: refactering

--- infrastructure/modules/lambda/variables.tf
+++ infrastructure/modules/lambda/variables.tf
@@ -20,13 +20,6 @@ locals {
     { "TZ" = "Asia/Tokyo" },
     var.envs
   )
-  ecr_repository_name = join("_", [
-    var.common_values.workspace,
-    var.name,
-    "lambda",
-    var.common_values.service,
-    var.common_values.project
-    ])
   func_dir = "${path.module}/../../functions/${var.name}"
   dist_dir = "${local.func_dir}/bootstrap"
 }
