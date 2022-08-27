4de994a41279855d2e243abd9cb66ae9069ea025
kenko <kyoshitomi.cursor@gmail.com>
2022-08-27 23:05:05 +0900

    fix name

--- infrastructure/service/main.tf
+++ infrastructure/service/main.tf
@@ -6,36 +6,36 @@ module "lambda" {
   name    = "greet"
 }
 
-module "apigwv2" {
-  source  = "../modules/apigwv2"
-  commons = local.commons
-  name    = "trial"
-}
-
-module "route" {
-  source      = "../modules/apigwv2/route"
-  apigateway  = module.apigwv2
-  lambda      = module.lambda
-  path_part   = "greet"
-  http_method = "POST"
-}
-
-# module "apigw" {
-#   source  = "../modules/apigw"
+# module "apigwv2" {
+#   source  = "../modules/apigwv2"
 #   commons = local.commons
 #   name    = "trial"
 # }
 
 # module "route" {
-#   source      = "../modules/apigw/route"
-#   apigateway  = module.apigw
+#   source      = "../modules/apigwv2/route"
+#   apigateway  = module.apigwv2
 #   lambda      = module.lambda
 #   path_part   = "greet"
 #   http_method = "POST"
 # }
 
-# module "develop" {
-#   source     = "../modules/apigw/develop"
-#   apigateway = module.apigw
-#   lambda     = module.lambda
-# }
+module "apigw" {
+  source  = "../modules/apigw"
+  commons = local.commons
+  name    = "trial"
+}
+
+module "route" {
+  source      = "../modules/apigw/route"
+  apigateway  = module.apigw
+  lambda      = module.lambda
+  path_part   = "greet"
+  http_method = "POST"
+}
+
+module "deploy" {
+  source     = "../modules/apigw/deploy"
+  apigateway = module.apigw
+  lambda     = module.lambda
+}
