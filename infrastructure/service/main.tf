fe3b4770a118907b075d9a830d756e6008761bff
kenko <kyoshitomi.cursor@gmail.com>
2022-08-28 00:26:57 +0900

    summarize lambda and route modules for apigw

--- infrastructure/service/main.tf
+++ infrastructure/service/main.tf
@@ -1,29 +1,40 @@
 data "aws_caller_identity" "_" {}
 
-module "lambda" {
-  source  = "../modules/lambda"
-  commons = local.commons
-  name    = "greet"
-}
-
 module "apigw" {
   source  = "../modules/apigw"
   commons = local.commons
   name    = "trial"
 }
 
-module "route" {
-  source      = "../modules/apigw/route"
-  apigateway  = module.apigw
-  lambda      = module.lambda
-  path_part   = "greet"
+module "lambda_integration" {
+  source      = "../modules/apigw/lambda_integration"
+  commons     = local.commons
+  name        = "greet"
+  apigw       = module.apigw
   http_method = "POST"
 }
+
+# module "lambda" {
+#   source  = "../modules/lambda"
+#   commons = local.commons
+#   name    = "greet"
+# }
+
+# module "route" {
+#   source      = "../modules/apigw/route"
+#   apigateway  = module.apigw
+#   lambda      = module.lambda
+#   path_part   = "greet"
+#   http_method = "POST"
+# }
 
 module "deploy" {
   source     = "../modules/apigw/deploy"
   apigateway = module.apigw
-  lambda     = module.lambda
+  lambda     = module.lambda_integration.lambda
+  depends_on = [
+    module.lambda_integration
+  ]
 }
 
 # module "apigwv2" {
