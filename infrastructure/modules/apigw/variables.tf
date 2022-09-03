cb83b3b867e19d89b6decbe63fdbe63abc9ea81e
kenko <kyoshitomi.cursor@gmail.com>
2022-09-04 08:49:01 +0900

    swap openapi v3 for api gateway

--- infrastructure/modules/apigw/variables.tf
+++ infrastructure/modules/apigw/variables.tf
@@ -1,8 +1,11 @@
 variable "commons" {}
-
 variable "name" {
   description = "resource name"
 }
+
+variable "body" {}
+variable "stage_name" {}
+variable "integrations" {}
 
 locals {
   name = join("_", [
@@ -11,4 +14,15 @@ locals {
     var.commons.service,
     var.commons.project
   ])
+  openapi_vars = {
+    for k, v in var.integrations :
+    k => v.function.invoke_arn
+  }
+  lambda_permissions = {
+    for k, v in var.integrations : k => {
+      function_name = v.function.function_name
+      http_method   = element(split("-", k), 0)
+      path_part     = element(split("-", k), 1)
+    }
+  }
 }
--- infrastructure/service/main.tf
+++ infrastructure/service/main.tf
@@ -1,18 +1,61 @@
 data "aws_caller_identity" "_" {}
 
-module "apigw" {
-  source  = "../modules/apigw"
+module "lambda_greet" {
+  source  = "../modules/lambda"
   commons = local.commons
-  name    = "trial"
+  name    = "greet"
 }
 
-module "lambda_integration" {
-  source      = "../modules/apigw/lambda_integration"
-  commons     = local.commons
-  name        = "greet"
-  apigw       = module.apigw
-  http_method = "POST"
+module "lambda_shortcut" {
+  source  = "../modules/lambda"
+  commons = local.commons
+  name    = "create_shortcut"
+  envs = {
+    "TABLE" : module.dynamodb.table.name
+  }
+  policy_statements = [
+    {
+      Action : [
+        "dynamodb:PutItem"
+      ],
+      Effect : "Allow",
+      Resource : module.dynamodb.table.arn
+    }
+  ]
 }
+
+module "dynamodb" {
+  source        = "../modules/dynamodb"
+  commons       = local.commons
+  name          = "shortcuts"
+  partition_key = "name"
+  attributes = [
+    {
+      name = "name"
+      type = "S"
+    }
+  ]
+}
+
+module "apigw" {
+  source     = "../modules/apigw"
+  commons    = local.commons
+  name       = "trial"
+  stage_name = "prod"
+  body       = file("openapi.yml")
+  integrations = {
+    "POST_greet"    = module.lambda_greet
+    "POST_shortcut" = module.lambda_shortcut
+  }
+}
+
+# module "lambda_integration" {
+#   source      = "../modules/apigw/lambda_integration"
+#   commons     = local.commons
+#   name        = "greet"
+#   apigw       = module.apigw
+#   http_method = "POST"
+# }
 
 # module "lambda" {
 #   source  = "../modules/lambda"
@@ -28,52 +71,39 @@ module "lambda_integration" {
 #   http_method = "POST"
 # }
 
-module "dynamodb" {
-  source        = "../modules/dynamodb"
-  commons       = local.commons
-  name          = "shortcuts"
-  partition_key = "name"
-  attributes = [
-    {
-      name = "name"
-      type = "S"
-    }
-  ]
-}
+# module "lambda_integration_create_shortcut" {
+#   source      = "../modules/apigw/lambda_integration"
+#   commons     = local.commons
+#   name        = "create_shortcut"
+#   apigw       = module.apigw
+#   http_method = "POST"
+#   path_part   = "shortcut"
+#   envs = {
+#     "TABLE" : module.dynamodb.table.name
+#   }
+#   policy_statements = [
+#     {
+#       Action : [
+#         "dynamodb:PutItem"
+#       ],
+#       Effect : "Allow",
+#       Resource : module.dynamodb.table.arn
+#     }
+#   ]
+# }
 
-module "lambda_integration_create_shortcut" {
-  source      = "../modules/apigw/lambda_integration"
-  commons     = local.commons
-  name        = "create_shortcut"
-  apigw       = module.apigw
-  http_method = "POST"
-  path_part   = "shortcut"
-  envs = {
-    "TABLE" : module.dynamodb.table.name
-  }
-  policy_statements = [
-    {
-      Action : [
-        "dynamodb:PutItem"
-      ],
-      Effect : "Allow",
-      Resource : module.dynamodb.table.arn
-    }
-  ]
-}
-
-module "deploy" {
-  source     = "../modules/apigw/deploy"
-  apigateway = module.apigw
-  lambdas = [
-    module.lambda_integration.lambda,
-    module.lambda_integration_create_shortcut.lambda
-  ]
-  depends_on = [
-    module.lambda_integration,
-    module.lambda_integration_create_shortcut
-  ]
-}
+# module "deploy" {
+#   source     = "../modules/apigw/deploy"
+#   apigateway = module.apigw
+#   lambdas = [
+#     module.lambda_integration.lambda,
+#     module.lambda_integration_create_shortcut.lambda
+#   ]
+#   depends_on = [
+#     module.lambda_integration,
+#     module.lambda_integration_create_shortcut
+#   ]
+# }
 
 # module "apigwv2" {
 #   source  = "../modules/apigwv2"
