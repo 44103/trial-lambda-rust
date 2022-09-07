9fbd1ab5ace532faa3a4b089236853e4c6f5ee69
kenko <kyoshitomi.cursor@gmail.com>
2022-09-07 17:23:31 +0900

    update lambda_http

--- infrastructure/functions/lambda/src/bin/create_quote.rs
+++ infrastructure/functions/lambda/src/bin/create_quote.rs
@@ -3,10 +3,14 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
+use std::env;
+use std::process;
 use lambda_http::{
   service_fn,
   IntoResponse, Request, Response,
 };
+use aws_sdk_dynamodb as dynamodb;
+use aws_sdk_dynamodb::model::AttributeValue;
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
@@ -19,7 +23,9 @@ async fn main() -> Result<(), Error> {
 #[derive(Debug, Deserialize, Default)]
 struct Args {
   #[serde(default)]
-  name: String
+  name: String,
+  #[serde(default)]
+  quote: String,
 }
 
 #[derive(Debug, Serialize, Default)]
@@ -30,7 +36,27 @@ struct Body {
 
 async fn func(event: Request) -> Result<impl IntoResponse, Error> {
   let args: Args = serde_json::from_slice(event.body().as_ref()).unwrap();
-  let body: Body = Body { message: format!("Hello {}", args.name) };
+
+  let config = aws_config::load_from_env().await;
+  let client = dynamodb::Client::new(&config);
+
+  let table = match env::var("TABLE") {
+    Ok(val) => val,
+    Err(err) => {
+      println!("{err}");
+      process::exit(1);
+    }
+  };
+  println!("{}", args.quote.clone());
+  client
+    .put_item()
+    .table_name(table)
+    .item("name", AttributeValue::S(args.name.clone()))
+    .item("quote", AttributeValue::S(args.quote.clone()))
+    .send().await?;
+
+  let body: Body = Body { message: format!("{} said, {}", args.name, args.quote) };
+
   Ok(Response::builder()
     .status(200)
     .header("Content-Type", "application/json")
